1c85ed62f86721ec49b65c4968b1666288d2e6c1
kunmiao.li <kunmiao.li@okg.com>
2023-07-19 16:07:36 +0800

    fix validator num

--- cleanAndstart.sh
+++ cleanAndstart.sh
@@ -2,6 +2,7 @@ rm -rf ./execution/geth
 rm -rf ./consensus/beacondata
 rm -rf ./consensus/genesis.ssz
 rm -rf ./consensus/validatordata
+rm -rf ./consensus/validatordata_custom
 docker-compose down
 rm -rf ./consensus/wallet
 docker-compose up -d
